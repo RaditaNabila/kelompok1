694e479e9898d11203f9ab8b8f46c91674b273d2
RaditaNabila <raditanabilaa@gmail.com>
2024-07-30 15:08:24 +0700

    menambahkan menu

--- tiket.cpp
+++ tiket.cpp
@@ -24,3 +24,14 @@ int main() {
     //pesan selamat datang
     cout << "=====================================" << endl;
     cout << "Selamat Datang di stasiun Indonesia" << endl;
+    cout << "Silahkan pilih tujuan anda" << endl;
+    cout << "=====================================" << endl;
+    cout << "*Tarif Per-Stasiun adalah Rp5000" << endl;
+    //menu
+    cout << "1. Stasiun Banjarmasin" << endl;
+    cout << "2. Stasiun Banjarbaru" << endl;
+    cout << "3. Stasiun Martapura" << endl;
+    cout << "4. Stasiun Kandangan" << endl;
+    cout << "5. Stasiun Kotabaru" << endl;
+
+    cout << endl;
